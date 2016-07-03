a5e958fceb44ed1a12b7a839e958d6a1fd32d771
Colin Davey <himself@colindavey.com>
2016-07-03 15:54:23 -0700

    Adds filter checkbox. Improves formatting, including index finger glyph.

--- main/myscript.js
+++ main/myscript.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function() {
 	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/10plQlO7bPpGm-IsXR53Lxxcr5a4z4J31JczBX28G6os/pubhtml';
 	Tabletop.init({
 		key: public_spreadsheet_url, 
-		callback: updateDOM, 
+		callback: initDataDOM, 
 		simpleSheet: true 
 	});
 });
+
+var jobsGlobal = [];
+var jobsFilteredGlobal = [];
+
+function toggleHiring() {
+	jQuery("#jobsList").empty();
+	updateDOM();
+}
 
 function myCompare(a,b) {
   var nameA = a.employer.toUpperCase(); // ignore upper and lowercase
@@ -25,22 +33,41 @@ function myCompare(a,b) {
   return 0;
 }
 
-function updateDOM(data, tabletop) {
+function initDataDOM(data, tabletop) {
 //	data = data.slice(0, 30);
 //	data.sort(function(a,b){return myCompare(a,b)});
 	data.sort(myCompare);
 //	console.log(JSON.stringify(data));
+ 	var numJobs = 0;
+ 	numJobsFilteredGlobal = 0;
+	var job;
+	for (var i = 0; i < data.length; i++) {
+		var tmpNumJobs = Number(data[i].openjobcount);
+		numJobs += tmpNumJobs;
+		// Not using tmpNumJobs here because need to distinguish between 0 and "". 
+		job = makeItem(data[i].employer, data[i].link, data[i].openjobcount, i+1);
 
-	var numCompanies = data.length;
-	var numJobs = 0;
-	var item;
+		jobsGlobal[jobsGlobal.length] = job;
+		if (tmpNumJobs > 0) {
+			jobsFilteredGlobal[jobsFilteredGlobal.length] = job;
+		}
+	}
+	jQuery("#numJobs").text(numJobs);
+	updateDOM();
+}
+
+function updateDOM() {
+	if (jQuery("#chkHiring").prop("checked")) {
+		var jobs = jobsFilteredGlobal;
+	} else {
+		var jobs = jobsGlobal;
+	}
+	
+	var numCompanies = jobs.length;
 	for (var i = 0; i < numCompanies; i++) {
-		numJobs += Number(data[i].openjobcount);
-		item = makeItem(data[i].employer, data[i].link, data[i].openjobcount, i+1);
-		jQuery("#jobsList").append(item);
+		jQuery("#jobsList").append(jobs[i]);
 	}
 	jQuery("#numCompanies").text(numCompanies);
-	jQuery("#numJobs").text(numJobs);
 }
 
 // makes and item that can be 0, 2, 3, or 4 columns. 
@@ -48,13 +75,22 @@ function updateDOM(data, tabletop) {
 function makeItem(employer, link, openjobcount, ind) {
 	item = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">';
 // 	item = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-lg-2">';
-// 	item += '<div class = row>';
+
+// 	item += '<div class="row">';
+// 	item += '<div class="clearfix">';
 	
-	item += '<div class="col-xs-1">';
-	item += ind.toString() + ") ";
+//  item += '<div class="col-xs-1">';
+// 	item += ind.toString() + ") ";
+// 	item += "*";
+
+//  item += '<div class="col-xs-2 text-center">';
+// 	item += '<span class="glyphicons glyphicons-hand-right" aria-hidden="true"></span>';
+// 	item += '</div>';
+	item += '<div class="col-xs-1 text-center">';
+	item += '<span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span>';
 	item += '</div>';
 
-	item += '<div class="col-xs-8">';
+	item += '<div class="col-xs-9">';
 // 	item += '<div class="col-xs-10">';
 	if (link === "") {
 		item += employer;
@@ -65,13 +101,19 @@ function makeItem(employer, link, openjobcount, ind) {
 	}
 	item += '</div>';
 
-	item += '<div class="col-xs-1">';
+// 	item += '<div class="col-xs-4">';
+ 	item += '<div class="col-xs-1">';
 	item += '<span class="highlight">';
-	item += openjobcount;
+	if (openjobcount === "") {
+		item += "-";
+	} else {
+		item += openjobcount;
+	}
 	item += '</span>';
 	item += '</div>';
 
-// 	item += '</div>';
+//  	item += '</div>';
+
 	item += '</li>';
 	return item;
 }
