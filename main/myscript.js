e9f35b35416cce66816361aea4abf59c6cb9f4b0
Colin Davey <himself@colindavey.com>
2016-07-03 16:37:32 -0700

    Starts to use JSON to get data from Google Doc.

--- main/myscript.js
+++ main/myscript.js
@@ -1,10 +1,28 @@
+// jQuery(document).ready(function() {
+// 	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/10plQlO7bPpGm-IsXR53Lxxcr5a4z4J31JczBX28G6os/pubhtml';
+// 	Tabletop.init({
+// 		key: public_spreadsheet_url, 
+// 		callback: initDataDOM, 
+// 		simpleSheet: true 
+// 	});
+// });
+
 jQuery(document).ready(function() {
-	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/10plQlO7bPpGm-IsXR53Lxxcr5a4z4J31JczBX28G6os/pubhtml';
-	Tabletop.init({
-		key: public_spreadsheet_url, 
-		callback: initDataDOM, 
-		simpleSheet: true 
-	});
+	var spreadsheetJson = 'https://spreadsheets.google.com/feeds/list/10plQlO7bPpGm-IsXR53Lxxcr5a4z4J31JczBX28G6os/od6/public/values?alt=json';
+    data = [];
+    jQuery.getJSON(spreadsheetJson, function(response){
+        // standardize data type for each company (1 company per row in the google docs spreadsheet)
+        data = _(response.feed.entry).map(function(cell){
+            return {
+                "name": cell.gsx$employer.$t + '',
+                "link": cell.gsx$link.$t + '',
+                "openings": cell.gsx$openjobcount.$t * 1
+            };
+            return company;
+        });
+        initDataDOM(data);
+        jQuery('#chkHiring').on('click',toggleHiring);
+    });
 });
 
 var jobsGlobal = [];
@@ -16,8 +34,8 @@ function toggleHiring() {
 }
 
 function myCompare(a,b) {
-  var nameA = a.employer.toUpperCase(); // ignore upper and lowercase
-  var nameB = b.employer.toUpperCase(); // ignore upper and lowercase
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
 //console.log(nameA + " " + nameB);
   if (nameA < nameB) {
 //console.log('<');
@@ -42,10 +60,10 @@ function initDataDOM(data, tabletop) {
  	numJobsFilteredGlobal = 0;
 	var job;
 	for (var i = 0; i < data.length; i++) {
-		var tmpNumJobs = Number(data[i].openjobcount);
+		var tmpNumJobs = Number(data[i].openings);
 		numJobs += tmpNumJobs;
 		// Not using tmpNumJobs here because need to distinguish between 0 and "". 
-		job = makeItem(data[i].employer, data[i].link, data[i].openjobcount, i+1);
+		job = makeItem(data[i].name, data[i].link, data[i].openings, i+1);
 
 		jobsGlobal[jobsGlobal.length] = job;
 		if (tmpNumJobs > 0) {
@@ -72,7 +90,7 @@ function updateDOM() {
 
 // makes and item that can be 0, 2, 3, or 4 columns. 
 // plus attempts to control space within item. 
-function makeItem(employer, link, openjobcount, ind) {
+function makeItem(name, link, openings, ind) {
 	item = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">';
 // 	item = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-lg-2">';
 
@@ -93,10 +111,10 @@ function makeItem(employer, link, openjobcount, ind) {
 	item += '<div class="col-xs-9">';
 // 	item += '<div class="col-xs-10">';
 	if (link === "") {
-		item += employer;
+		item += name;
 	} else {
 		item += '<a href="'; item += link; item += '" TARGET="link">';
-		item += employer;
+		item += name;
 		item += '</a>';
 	}
 	item += '</div>';
@@ -104,10 +122,10 @@ function makeItem(employer, link, openjobcount, ind) {
 // 	item += '<div class="col-xs-4">';
  	item += '<div class="col-xs-1">';
 	item += '<span class="highlight">';
-	if (openjobcount === "") {
+	if (openings === "") {
 		item += "-";
 	} else {
-		item += openjobcount;
+		item += openings;
 	}
 	item += '</span>';
 	item += '</div>';
@@ -119,18 +137,18 @@ function makeItem(employer, link, openjobcount, ind) {
 }
 
 // makes and item that can be 0, 2, 3, or 4 columns. 
-// function makeItem(employer, link, openjobcount, ind) {
+// function makeItem(name, link, openings, ind) {
 // 	item = '<li class="col-xs-12 col-sm-6 col-md-4 col-lg-3">';
 // 	if (link === "") {
-// 		item += employer;
+// 		item += name;
 // 	} else {
 // 		item += '<a href="'; item += link; item += '" TARGET="link">';
-// 		item += employer;
+// 		item += name;
 // 		item += '</a>';
 // 	}
 // 	item += ' ';
 // 	item += '<span class="highlight">';
-// 	item += openjobcount;
+// 	item += openings;
 // 	item += '</span>';
 // 	item += '</li>';
 // 	return item;
